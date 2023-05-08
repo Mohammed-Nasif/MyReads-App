1b8c87de8ec6f1ec43648a2378ef3b27a91b8456
Mohammed Nasif <m.khaled100@ymail.com>
2023-05-08 15:36:23 +0300

    Search Shelf PlaceHolder

--- src/components/BooksShelf.tsx
+++ src/components/BooksShelf.tsx
@@ -7,30 +7,45 @@ import { Book } from './Book';
 // Types and Interfaces
 import { Book as BookInterface, Shelf } from '../@types/interfaces';
 
+// LottieFiles
+import { Player } from '@lottiefiles/react-lottie-player';
+
 export const BooksShelf: FC<{
 	shelf: Shelf;
 	shelvesBooks: BookInterface[];
 }> = ({ shelf, shelvesBooks }): JSX.Element => {
-	
+
 	return (
 		<div className='bookshelf'>
 			<h2 className='bookshelf-title mt-3 pb-2'>{shelf.name}</h2>
 			<div className='bookshelf-books'>
 				<ol className='books-grid'>
 					{/* Conditional Render For Search Shelf casue there is no book.shelf === Search so No Filter Applied */}
-					{shelf.identifier === 'search'
-						? shelvesBooks.map((book: BookInterface) => (
+					{shelf.identifier === 'search' ? (
+						shelvesBooks.length > 0 ? (
+							shelvesBooks.map((book: BookInterface) => (
 								<li key={book.id}>
 									<Book book={book} />
 								</li>
-						  ))
-						: shelvesBooks
-								.filter((book: BookInterface) => book.shelf === shelf.identifier)
-								.map((book: BookInterface) => (
-									<li key={book.id}>
-										<Book book={book} />
-									</li>
-								))}
+							))
+						) : (
+							<Player
+								src='https://assets4.lottiefiles.com/packages/lf20_MrIjH2.json'
+								className='player'
+								loop
+								autoplay
+								style={{ height: '500px', width: '500px', paddingTop: '50px' }}
+							/>
+						)
+					) : (
+						shelvesBooks
+							.filter((book: BookInterface) => book.shelf === shelf.identifier)
+							.map((book: BookInterface) => (
+								<li key={book.id}>
+									<Book book={book} />
+								</li>
+							))
+					)}
 				</ol>
 			</div>
 		</div>
